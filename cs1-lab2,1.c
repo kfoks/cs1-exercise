4cf1fe2c44f425d89b9c99932ba84df2aa47ec8b
kfoks <91980132+kfoks@users.noreply.github.com>
2021-10-20 13:43:21 +0200

    Create cs1-lab2,1.c

---
+++ cs1-lab2,1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+void Printer();
+
+int main()
+{
+	Printer();
+
+	return 0;
+	
+}
+
+void Printer()
+{
+	printf("Hello, I am a program, my name is Printer. I print stuff! \n");
+
+}
